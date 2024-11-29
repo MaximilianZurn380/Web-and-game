d2e331cc13bc25ac8822325331c077abce641dd6
James <mazua001@gmail.com>
2024-11-29 10:53:42 +0100

    lagde en database

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import sqlite3
 
 app = Flask(__name__)
+
+
+def function_db():
+    conn = sqlite3.connect("scores.db")
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS scores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            player_name TEXT NOT NULL,
+            score INTEGER NOT NULL
+        )
+    ''')
+    conn.commit
+    conn.close
+
+function_db()
