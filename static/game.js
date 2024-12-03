10c35164ca6890f5b745e9dbbd822188d8fac63e
James <mazua001@gmail.com>
2024-12-03 11:12:30 +0100

    lets the user save their score

--- static/game.js
+++ static/game.js
@@ -14,7 +14,26 @@ document.getElementById("saveScore").addEventListener("click", function () {
         alert("Please enter your name");
         return;
     }
-})
+
+
+    fetch("/save-score", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            player_name: playerName,
+            score: score
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+    })
+    .catch(error => {
+        console.error("Error", error);
+    });
+});
 document.getElementById("saveScore").addEventListener("click", function ()
 {
     console.log("eghejghjkgfh");
